94f6de282d8acf4b33ff2f660fbec1fe24fb831a
Ahmed Essam <ahmedessamahmedmohamed@gmail.com>
2021-01-14 19:25:07 +0200

    .

--- resources/views/exports/products.blade.php
+++ resources/views/exports/products.blade.php
@@ -14,15 +14,15 @@
     </tr>
     </thead>
     <tbody>
-        @foreach($products as $product)
+        @foreach($products as $key => $product)
             <tr>
                 <td>
-                    @if(is_file(public_path('storage/uploads/products/main/'.$product->images[0]->large_image)))
-                        @if(substr(strrchr($product->images[0]->large_image,'.'),1) === 'jpg')
+                    @if($key < 5)
+                        @if(is_file(public_path('storage/uploads/products/main/'.$product->images[0]->large_image)))
                             <img height="80px" width="80px" src="{{ public_path('storage/uploads/products/main/'.$product->images[0]->large_image) }}" alt="">
+                        @else
+                            <img height="80px" width="80px" src="{{ public_path() }}/assets/placeholder.png" alt="">
                         @endif
-                    @else
-                        <img height="80px" width="80px" src="{{ public_path() }}/assets/placeholder.png" alt="">
                     @endif
                 </td>
                 <td>{{ $product->name }}</td>
